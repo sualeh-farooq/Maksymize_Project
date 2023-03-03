d387cc843b84311af5b159f798408eba2c5b02be
Yaqoob <sualehfarooq65@gmail.com>
2023-03-04 02:14:12 +0500

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,6 @@
 import Head from 'next/head'
 import { Inter } from 'next/font/google'
-import { HomePage } from './home'
-// import Swiper from '@s'
-// import 'swiper/css'
+import HomePage from './home'
 
 
 // const swiper = new Swiper()
