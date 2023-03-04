4c7fb5c2cba073bd270c1cbe062a4ec264a2a9b9
Yaqoob <sualehfarooq65@gmail.com>
2023-03-04 05:35:56 +0500

    commit

--- src/containers/contact/index.js
+++ src/containers/contact/index.js
@@ -1,18 +1,9 @@
 import { Submit_Button } from "@/components/buttons";
-import Head from "next/head";
-import Script from "next/script";
 export function Contact_Section() {
+
   return (
     <>
-     {/* <Head>
-        <link
-          href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
-          rel="stylesheet"
-          integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
-          crossOrigin="anonymous"
-        />
-        </Head> */}
-      <div className="approach-bg h-96 order-1 z-0" style={{ height: "65rem" }}>
+      <div className="approach-bg h-96 order-1 z-0" style={{ height: "80rem" }}>
         <div className="w-full flex items-center justify-center ">
           <div className="absolute top-36 w-full approach-bg  rounded py-12 lg:px-28 px-8">
             <p className="md:text-lg md:mb-10 text-md uppercase font-bold leading-7 text-center text-red-500">
@@ -73,10 +64,6 @@ export function Contact_Section() {
           </div>
         </div>
       </div>
-      <Script
-        id="bootstrap-cdn"
-        src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-      />
     </>
   );
 }
