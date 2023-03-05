4e5c0689b739efe4d2d43f0f5b8d7c15be18a3d9
Hasham-dev <vakanihasham64@gmail.com>
2023-03-05 17:55:07 +0500

    fix: fixes the build issue

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,6 @@
 import Head from "next/head";
 import { Inter } from "next/font/google";
-import HomePage from "./home";
-function classNames(...classes) {
-  return classes.filter(Boolean).join(" ");
-}
-const inter = Inter({ subsets: ["latin"] });
+import HomePage from "@/components/Home";
 
 export default function Home() {
   return (
